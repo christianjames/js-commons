de567eec1a5278c22b672f8fc239fa06716dd5ea
Christian James <contato@christianjames.com.br>
2015-12-15 16:12:40 -0200

    add getExtensionFile
    add getExtensionFile

--- custom-functions.js
+++ custom-functions.js
@@ -1,5 +1,11 @@
 CustomFunctions = {
     getdaysInMonth: function (month,year) { 
         return new Date(year, month, 0).getDate(); 
+    },
+    getExtensionFile: function (Caminho){
+        Caminho     = Caminho.replace('/\/g', "/");
+        var Arquivo = Caminho.substring(Caminho.lastIndexOf('/') + 1);
+        var Extensao= Arquivo.substring(Arquivo.lastIndexOf('.') + 1);
+        return {arquivo:Arquivo, extensao:Extensao};
     }
 }
