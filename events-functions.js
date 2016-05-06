0c309f50aab6ea0550c97bc78424c656ead9089a
Christian James <contato@christianjames.com.br>
2016-05-06 11:23:32 -0300

    Create events-functions.js
    The purpose of this file is to make the event handling.

---
+++ events-functions.js
@@ -0,0 +1,47 @@
+var EventsFunction = (function() {
+    'use strict';
+ 
+    var $public = {};
+    var $private = {};
+ 
+    //Usado em pesquisar ou autocomplete, pois ele executa depois de um delay apos a ultima teclada
+    $public.debounce = (function () {
+        //Tempo em ms
+        var timeWindow = 500,
+            timeout;
+
+        var implementation = function (callback) {
+            callback();
+        };
+
+        return function() {
+            var context = this;
+            var args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function(){
+                implementation.apply(context, args);
+            }, timeWindow);
+        };
+    }());
+
+    //Usado geralmente em cliques, para evitar cliques duplos
+    $public.throttle = (function () {
+
+        var timeWindow = 500; // tempo em ms
+        var lastExecution = new Date((new Date()).getTime() - timeWindow);
+
+        var implementation = function (callback) {
+            callback();
+        };
+
+        return function() {
+            if ((lastExecution.getTime() + timeWindow) <= (new Date()).getTime()) {
+                lastExecution = new Date();
+                return implementation.apply(this, arguments);
+            }
+        };
+    }());
+ 
+    return $public;
+ 
+})();
