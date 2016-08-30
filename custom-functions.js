602f3866cd38e87813351044001c7d0df9c67a51
Christian James <contato@christianjames.com.br>
2016-08-30 11:51:40 -0300

    Update custom-functions.js

--- custom-functions.js
+++ custom-functions.js
@@ -7,5 +7,33 @@ CustomFunctions = {
         var Arquivo = Caminho.substring(Caminho.lastIndexOf('/') + 1);
         var Extensao= Arquivo.substring(Arquivo.lastIndexOf('.') + 1);
         return {arquivo:Arquivo, extensao:Extensao};
+    },
+    getDimensionsKeepRatio: function (lastWidth, lastHeight, newWidth, newHeight) {
+            var ratioW = lastWidth / newWidth;
+            var ratioH = lastHeight / newHeight;
+            
+            var ratio;
+            if ((ratioW <= 1 && ratioH <= 1) || (ratioW >= 1 && ratioH >= 1))
+            {
+                ratio = 1 / ((ratioW > ratioH) ? ratioW : ratioH);
+            }
+            else if (ratioW > 1 && ratioH <= 1)
+            {
+                ratio = 1 / ratioW;
+            }
+            else if (ratioW <= 1 && ratioH > 1)
+            {
+                ratio = 1 / ratioH;
+            }
+
+            if (ratio) {
+                lastWidth *= ratio;
+                lastHeight *= ratio;
+            }
+
+            return {
+                width : lastWidth,
+                height : lastHeight
+            };
     }
 }
