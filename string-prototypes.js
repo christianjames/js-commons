6be839f59bd7a2c4e54628dc1ac66bfb397b1239
Christian James <contato@christianjames.com.br>
2016-10-17 14:40:08 -0200

    Update string-prototypes.js

--- string-prototypes.js
+++ string-prototypes.js
@@ -1,3 +1,20 @@
+String.prototype.linkify = function() {
+
+// http://, https://, ftp://
+var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+
+// www. sans http:// or https://
+var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+
+// Email addresses
+var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+
+return this
+    .replace(urlPattern, '<a href="$&">$&</a>')
+    .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+    .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+};
+
 String.prototype.toTileCase = function () {
     return this.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
 };
