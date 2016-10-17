13aafa10daec7751a501e790042555e15719bf2d
Christian James <contato@christianjames.com.br>
2016-10-17 15:00:20 -0200

    Update string-prototypes.js

--- string-prototypes.js
+++ string-prototypes.js
@@ -1,18 +1,21 @@
-String.prototype.linkify = function() {
+String.prototype.linkify = function(options) {
+    var options = options || {};
+    
+    options.target = options.target || '_self';
 
-// http://, https://, ftp://
-var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
+    // http://, https://, ftp://
+    var urlPattern = /\b(?:https?|ftp):\/\/[a-z0-9-+&@#\/%?=~_|!:,.;]*[a-z0-9-+&@#\/%=~_|]/gim;
 
-// www. sans http:// or https://
-var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    // www. sans http:// or https://
+    var pseudoUrlPattern = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
 
-// Email addresses
-var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
+    // Email addresses
+    var emailAddressPattern = /[\w.]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,6})+/gim;
 
-return this
-    .replace(urlPattern, '<a href="$&">$&</a>')
-    .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
-    .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
+    return this
+        .replace(urlPattern, '<a href="$&">$&</a>')
+        .replace(pseudoUrlPattern, '$1<a href="http://$2">$2</a>')
+        .replace(emailAddressPattern, '<a href="mailto:$&">$&</a>');
 };
 
 String.prototype.toTileCase = function () {
